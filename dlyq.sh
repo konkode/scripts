812703605b24728e96d65f83bb0d1f907f6df153
konkode <44456824+konkode@users.noreply.github.com>
2021-06-08 21:06:19 -0400

    dlyq: improve readability

--- dlyq.sh
+++ dlyq.sh
@@ -16,7 +16,7 @@ asRoot='sh -c'
         elif exists su; then
             asRoot='su -c'
         else
-            printf "\033[1;31mCan't get permission to install to system-wide directory. Quitting.\033[m\n"
+            printf "\033[1;31mCan't get permission to install to system directory. Quitting.\033[m\n"
             exit 1
         fi
     fi
@@ -24,12 +24,14 @@ asRoot='sh -c'
 link="$(curl https://github.com/mikefarah/yq/releases/latest 2>/dev/null | grep -o '"http.*"')"
 version="$(echo "$link" | grep -o 'v[0-9].*[0-9]')"
 
-install() {
 echo "Downloading from ${link}"
-printf "Installing yq %s for linux_amd64 to /usr/local/bin/yq\n" "$version"
-wget https://github.com/mikefarah/yq/releases/download/"${version}"/yq_linux_amd64.tar.gz -O - 2>/dev/null | tar xz &&
-$asRoot "mv yq_linux_amd64 /usr/local/bin/yq" &&
-echo "Done!"
+
+install() {
+    wget https://github.com/mikefarah/yq/releases/download/"${version}"/yq_linux_amd64.tar.gz -O - 2>/dev/null | tar xz &&
+    $asRoot "mv yq_linux_amd64 /usr/local/bin/yq"
 }
 
-install
+printf "Installing yq %s for linux_amd64 to /usr/local/bin/yq\n" "$version"
+
+install &&
+echo "Done!"
