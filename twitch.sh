29d796c4689397d64cd4776a082cab999999dbf7
konkode <44456824+konkode@users.noreply.github.com>
2021-06-30 02:04:54 -0400

    twitch: case for quality settings

--- twitch.sh
+++ twitch.sh
@@ -1,3 +1,19 @@
 #!/bin/sh
 
-mpv https://twitch.tv/"$1" > /dev/null 2>&1 &
+if [ -z "$2" ] ; then
+    quality="best"
+else
+    case $2 in
+        [7]|[7][2][0]* )
+            quality="best[height<=720]"
+            ;;
+        [4]|[4][8][0]* )
+            quality="best[height<=480]"
+            ;;
+        [3]|[3][6][0]* )
+            quality="best[height<=360]"
+            ;;
+    esac
+fi
+
+mpv --ytdl-format="$quality" https://twitch.tv/"$1" > /dev/null 2>&1 &
